055cc8a6c5f210edfd052931710a3e5a81024e9f
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-04-08 11:08:51 +0200

    Add script to install all QtCreator schemes
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ scripts/install-QtCreator.py
@@ -0,0 +1,50 @@
+import os
+import requests
+from sys import platform
+from shutil import copyfile
+from pathlib import Path
+
+
+def schemeDir():
+    dir = ""
+    if platform == "linux" or platform == "linux2":
+        dir = str(Path.home()) + "/.config/QtProject/qtcreator/styles/"
+
+    return dir;
+
+
+def copy(filename):
+    if not os.path.exists(schemeDir()):
+        os.mkdir(schemeDir())
+
+    copyfile(filename, schemeDir() + os.path.basename(filename))
+
+
+def download(filename):
+    link = ""
+    with open(filename) as f:
+        link = f.readline()
+    
+    file = requests.get(link)
+    open(schemeDir() + os.path.basename(filename) + ".xml", 'wb').write(file.content)
+
+
+def install(filename):
+    if filename.endswith(".xml"):
+        copy(filename)
+    else:
+        download(filename)
+
+
+def main():
+    print("installing QtCreator color schemes...")
+    QtCreatorPath = "./QtCreator/"
+    (_, _, filenames) = next(os.walk(QtCreatorPath))
+    for item in filenames:
+        install(QtCreatorPath + item)
+
+    print("install finished")
+
+
+if __name__ == "__main__":
+    main()
